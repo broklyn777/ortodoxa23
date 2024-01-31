9eb0d926c77b0fef403fd7c9c15afb913d46ed39
broklyn777 <50321354+broklyn777@users.noreply.github.com>
2024-01-31 15:24:05 +0100

    synax page

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,7 +1,7 @@
 const headerNavLinks = [
   { href: '/blog', title: 'Blogg' },
   { href: '/tags', title: 'Tags' },
-  { href: '/synaxarium', title: 'Synaxarium' },
+  // { href: '/synaxarium', title: 'Synaxarium' },
   // { href: '/projekt-light', title: 'projekt-light' },
   // { href: '/projekt', title: 'projektxx' },
   { href: '/projects', title: 'Projekt' },
