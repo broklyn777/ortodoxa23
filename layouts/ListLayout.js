7b3ccacb70f39f765ef0780ff275efd90f9f4a21
broklyn777 <50321354+broklyn777@users.noreply.github.com>
2024-02-02 22:53:40 +0100

    acedia

--- layouts/ListLayout.js
+++ layouts/ListLayout.js
@@ -8,7 +8,8 @@ import formatDate from '@/lib/utils/formatDate'
 export default function ListLayout({ posts, title, initialDisplayPosts = [], pagination }) {
   const [searchValue, setSearchValue] = useState('')
   const filteredBlogPosts = posts.filter((frontMatter) => {
-    const searchContent = frontMatter.title + frontMatter.summary + frontMatter.tags.join(' ')
+    const searchContent =
+      frontMatter.title + frontMatter.summary + (frontMatter.tags ? frontMatter.tags.join(' ') : '')
     return searchContent.toLowerCase().includes(searchValue.toLowerCase())
   })
 
@@ -68,9 +69,7 @@ export default function ListLayout({ posts, title, initialDisplayPosts = [], pag
                         </Link>
                       </h3>
                       <div className="flex flex-wrap">
-                        {tags.map((tag) => (
-                          <Tag key={tag} text={tag} />
-                        ))}
+                        {tags && tags.map((tag) => <Tag key={tag} text={tag} />)}
                       </div>
                     </div>
                     <div className="prose max-w-none text-gray-500 dark:text-gray-400">
